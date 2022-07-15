aeab4c35b1fe18afaa9cdc7bec7100e1812437be
NGANGAWAMBUI <105638396+NGANGAWAMBUI@users.noreply.github.com>
2022-07-15 15:32:38 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Gitongas factory\n");
+    printf main(mary wambui/);
+    printf(kra14356789/);
+    printf(worked hours>6hours);
+    scanf(each employee=30%\100);
+    scanf(normal pay =ksh1000);
+    scan f(overpay =1000+1.5*1000);
+    scanf(taxpaid 30%*1000)
+    return 0;
+}
